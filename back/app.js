06d01429ec923d75e2fec720955a8c3f1324e855
alberto gomez marin <albertogomezmarin@albertos-MacBook-Air.local>
2020-04-01 20:29:19 +0200

    routes sign up

--- back/app.js
+++ back/app.js
@@ -3,14 +3,18 @@ require("dotenv").config();
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const express = require("express");
-const favicon = require("serve-favicon");
-const hbs = require("hbs");
 const mongoose = require("mongoose");
 const logger = require("morgan");
 const path = require("path");
+const cors = require("cors");
+const session = require("express-session");
+const MongoStore = require("connect-mongo")(session);
 
 mongoose
-  .connect("mongodb://localhost/back", { useNewUrlParser: true })
+  .connect(process.env.DBURL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
   .then(x => {
     console.log(
       `Connected to Mongo! Database name: "${x.connections[0].name}"`
@@ -27,31 +31,43 @@ const debug = require("debug")(
 
 const app = express();
 
+// Cross Domain CORS whitlist
+const whitelist = ["http://localhost:3000", "http://localhost:1234"];
+const corsOptions = {
+  origin: function(origin, callback) {
+    console.log(`Origin: ${origin}`);
+    if (!origin) return callback(null, true);
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true
+};
+
 // Middleware Setup
+app.use(cors(corsOptions));
 app.use(logger("dev"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-
-// Express View engine setup
-
 app.use(
-  require("node-sass-middleware")({
-    src: path.join(__dirname, "public"),
-    dest: path.join(__dirname, "public"),
-    sourceMap: true
+  session({
+    secret: "keyboard cat",
+    resave: true,
+    saveUninitialized: true,
+    store: new MongoStore({ mongooseConnection: mongoose.connection })
   })
 );
+require("./passport")(app);
 
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "hbs");
 app.use(express.static(path.join(__dirname, "public")));
-app.use(favicon(path.join(__dirname, "public", "images", "favicon.ico")));
-
-// default value for title local
-app.locals.title = "Express - Generated with IronGenerator";
 
 const index = require("./routes/index");
 app.use("/", index);
+
+const auth = require("./routes/auth");
+app.use("/auth", auth);
 
 module.exports = app;
--- back/models/User.js
+++ back/models/User.js
@@ -1,24 +1,20 @@
-const mongoose = require('mongoose');
-const bcrypt = require('bcryptjs');
-​
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+
 const SALT_WORK_FACTOR = 10;
 const IMAGE_URL = /(http(s?):)([/|.|\w|\s|-])*\.(?:jpg|gif|png|svg)/g;
-const EMAIL_PATTERN= /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+const EMAIL_PATTERN = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 const PASSWORD_PATTERN = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{4,8}$/;
-​
 const userSchema = new mongoose.Schema(
   {
     name: {
       type: String,
-      required: 'Name is required',
+      required: "Name is required"
     },
     email: {
       type: String,
       trim: true,
-      match: [
-        EMAIL_PATTERN,
-        'Please fill a valid email address',
-      ],
+      match: [EMAIL_PATTERN, "Please fill a valid email address"],
       sparse: true,
       unique: false,
       default: null,
@@ -26,17 +22,17 @@ const userSchema = new mongoose.Schema(
     },
     password: {
       type: String,
-      required: [true, 'Password is required'],
-      match: [PASSWORD_PATTERN, 'Invalid password pattern'],
-    },
-    avatar: {
-      type: String,
-      default: 'https://www.ibts.org/wp-content/uploads/2017/08/iStock-476085198.jpg',
-    },
-    social: {
-      google: String,
-      facebook: String,
+      required: [true, "Password is required"],
+      match: [PASSWORD_PATTERN, "Invalid password pattern"]
     }
+    // avatar: {
+    //   type: String,
+    //   default: 'https://www.ibts.org/wp-content/uploads/2017/08/iStock-476085198.jpg',
+    // },
+    // social: {
+    //   google: String,
+    //   facebook: String,
+    // }
   },
   {
     timestamps: true,
@@ -49,9 +45,9 @@ const userSchema = new mongoose.Schema(
         delete ret.updatedAt;
         delete ret.__v;
         return ret;
-      },
-    },
-  },
+      }
+    }
+  }
 );
 // ​
 // userSchema.pre('save', function (next) {
@@ -71,8 +67,5 @@ const userSchema = new mongoose.Schema(
 //     })
 //     .catch(error => next(error));
 // });
-​
-​
-const User = mongoose.model('User', userSchema);
-​
+const User = mongoose.model("User", userSchema);
 module.exports = User;
