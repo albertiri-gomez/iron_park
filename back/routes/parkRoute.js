3f7f6683af908c8dc67f95de651b9589d292d7d8
alberto gomez marin <albertogomezmarin@albertos-MacBook-Air.local>
2020-04-02 22:07:00 +0200

    parkRoutes

---
+++ back/routes/parkRoute.js
@@ -0,0 +1,69 @@
+const express = require("express");
+const router = express.Router();
+const { isLoggedIn, isLoggedOut } = require("../lib/isLoggedMiddleware");
+const ensureLogin = require("connect-ensure-login");
+const User = require("../models/User");
+const Park = require("../models/Park");
+const Review = require("../models/Reviews");
+const mongoose = require("mongoose");
+
+// new review
+router.post("/review", async (req, res, next) => {
+  try {
+    const { starts = 0, comment } = req.body;
+    await Review.create({
+      park: mongoose.Types.ObjectId(req.body.park),
+      user: mongoose.Types.ObjectId(req.user.id),
+      rates: {
+        starts
+      },
+      comment
+    });
+    return res.json(`${req.body.park}`);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+// API that serves the name of all the temples
+router.post("/get-names", async (req, res, next) => {
+  try {
+    const parks = await Park.aggregate([
+      {
+        $project: {
+          _id: 0,
+          name: 1
+        }
+      }
+    ]);
+    return res.json(parks);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+// park details
+router.get("/:id", (req, res, next) => {
+  Park.findById(req.params.id)
+    .then(async park => {
+      const reviews = await Review.find({
+        park
+      })
+        .populate("user")
+        .sort({
+          createdAt: -1
+        });
+      const savedFavorite = isparkFavorite(req.user, park._id);
+      return res.render("park", {
+        park,
+        reviews,
+        savedFavorite
+      });
+    })
+    .catch(error => {
+      console.log(error);
+      next();
+    });
+});
+
+module.exports = router;
