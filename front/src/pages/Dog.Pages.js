e2662ef6086fe58ccb0a13fae3f83f0c807dc6f8
alberto gomez marin <albertogomezmarin@albertos-MacBook-Air.local>
2020-04-13 18:43:54 +0200

    meeting finish

--- front/src/pages/Dog.Pages.js
+++ front/src/pages/Dog.Pages.js
@@ -1,37 +1,51 @@
-import React, { useState, useContext } from "react";
+import React, { useState, useEffect } from "react";
 import { Card, Button } from "react-bootstrap";
 import { ApiContext } from "../../context/ApiContext";
 import { withRouter } from "react-router-dom";
 import { getDogs } from "../../lib/dog.api";
 
-export const DogPages = withRouter(({ history }) => {
-  const { dog, setDog } = useState(getDogs);
+export const DogPages = (props) => {
+  const { dogs, setDogs } = useState([]);
   console.log();
+
+  useEffect(() => {
+    getDogs(props).then((dog) => setDogs(dog));
+  }, []);
   // const { user, setUser } = useContext(ApiContext);
 
-  const onClick = async (e) => {
-    e.preventDefault();
-    await getDogs();
-    setUser(null);
-    history.push("/");
-  };
+  // const onClick = async (e) => {
+  //   e.preventDefault();
+  //   await getDogs();
+  //   setUser(null);
+  //   history.push("/");
+  // };
 
   return (
-    <Card style={{ width: "18rem" }}>
-      {dog.map((dogs) => {
-        return (
-          <>
-            <Card.Img variant="top" src={dogs.image} />
-            <Card.Body>
-              <Card.Title>{dogs.dogName}</Card.Title>
-              <Card.Text>{dogs.description}</Card.Text>
-              <Button variant="primary">Go somewhere</Button>
-            </Card.Body>
-          </>
-        );
-      })}
-    </Card>
+    <>
+      <Card style={{ width: "18rem" }}>
+        {dogs.map((dog) => {
+          return (
+            <>
+              {/* <Card.Img variant="top" src={dogs.image} /> */}
+              <Card.Body>
+                <Card.Title>{dog.dogName}</Card.Title>
+                <Card.Text>{dog.description}</Card.Text>
+                <Card.Text>{dog.race}</Card.Text>
+                {/* <Button variant="primary">Go somewhere</Button> */}
+              </Card.Body>
+            </>
+          );
+        })}
+      </Card>
+      <Nav defaultActiveKey="/home" as="ul">
+        <Nav.Item as="li">
+          <Nav.Link as="div">
+            <Link to="/create_dog">Crear Peroo</Link>
+          </Nav.Link>
+        </Nav.Item>
+      </Nav>
+    </>
   );
-});
+};
 
 // dogName, race, description, image
--- front/src/pages/Meetings.Pages.js
+++ front/src/pages/Meetings.Pages.js
@@ -1,18 +1,41 @@
-import React, { useState, useContext } from "react";
+import React, { useState, useEffect } from "react";
 import { Card, Button } from "react-bootstrap";
-import { getDogs } from "../../lib/dog.api";
+import { getMeeting } from "../../lib/meeting.api";
 import { ApiContext } from "../../context/ApiContext";
 import { withRouter, Link } from "react-router-dom";
-import { Navbar, Nav } from "react-bootstrap";
+import { Nav } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-export const MeetingsPages = () => {
+export const MeetingsPages = (props) => {
+  const [meetings, setMeeting] = useState([]);
+
+  useEffect(() => {
+    getMeeting(props).then((meeting) => setMeeting(meeting));
+  }, []);
   return (
-    <Nav defaultActiveKey="/home" as="ul">
-      <Nav.Item as="li">
-        <Nav.Link as="div">
-          <Link to="/create_meeting">Crear reunion</Link>
-        </Nav.Link>
-      </Nav.Item>
-    </Nav>
+    <>
+      <Card style={{ width: "18rem" }}>
+        {meetings.map((meeting) => {
+          return (
+            <>
+              {/* <Card.Img variant="top" src={dogs.image} /> */}
+              <Card.Body>
+                <Card.Title>{meeting.nameMeeting}</Card.Title>
+                <Card.Text>{meeting.description}</Card.Text>
+                <Card.Text>{meeting.date}</Card.Text>
+                {/* <Button variant="primary">Go somewhere</Button> */}
+              </Card.Body>
+            </>
+          );
+        })}
+      </Card>
+      <Nav defaultActiveKey="/home" as="ul">
+        <Nav.Item as="li">
+          <Nav.Link as="div">
+            <Link to="/create_meeting">Crear reunion</Link>
+          </Nav.Link>
+        </Nav.Item>
+      </Nav>
+    </>
   );
 };
