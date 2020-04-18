9fb8a745f10bbe4f6cc441e8d94152ee231e1327
alberto gomez marin <albertogomezmarin@albertos-MacBook-Air.local>
2020-04-18 14:07:04 +0200

    itera

--- front/src/pages/Map.pages.js
+++ front/src/pages/Map.pages.js
@@ -1,15 +1,15 @@
-import React, { useState } from "react";
-import { MapView } from "../components/Maps";
-import { Token } from "../../token_key";
-import { LoadScript } from "@react-google-maps/api";
-import { Token } from "../../token/token_key";
+// import React, { useState } from "react";
+// import { MapView } from "../components/Maps";
+// import { Token } from "../../token_key";
+// import { LoadScript } from "@react-google-maps/api";
+// import { Token } from "../../token/token_key";
 
-export const MapPages = () => {
-  return (
-    <>
-      <LoadScript id="map-google-load" googleMapsApiKey={Token()}>
-        <MapView></MapView>
-      </LoadScript>
-    </>
-  );
-};
+// export const MapPages = () => {
+//   return (
+//     <>
+//       <LoadScript id="map-google-load" googleMapsApiKey={Token()}>
+//         <MapView></MapView>
+//       </LoadScript>
+//     </>
+//   );
+// };
