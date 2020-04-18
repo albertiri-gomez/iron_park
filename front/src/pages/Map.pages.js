efa9fc37e5b07064d3a2bf2f3ceec90fee7d897e
alberto gomez marin <albertogomezmarin@albertos-MacBook-Air.local>
2020-04-18 14:04:20 +0200

    herok put

--- front/src/pages/Map.pages.js
+++ front/src/pages/Map.pages.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { MapView } from "../components/Maps";
 import { Token } from "../../token_key";
 import { LoadScript } from "@react-google-maps/api";
+import { Token } from "../../token/token_key";
 
 export const MapPages = () => {
   return (
